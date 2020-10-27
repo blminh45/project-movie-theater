39e0a66993767285d787624ab3482d7fca3b3527
blminh <0306181245@caothang.edu.vn>
2020-10-28 00:02:16 +0700

    update PayActivity

--- Movie-App/app/src/main/java/com/example/movieapp/MainActivity.java
+++ Movie-App/app/src/main/java/com/example/movieapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.movieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,PayActivity.class);
+                ActivityOptions options;
+                options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this);
+                startActivity(intent,options.toBundle());
+                finish();
+            }
+        },200);
     }
 }
