eb7a542a8662ac31c67cf2e7824a0a6ca7dfc20a
Tuns <0306181290@caothang.edu.vn>
2020-10-28 10:47:29 +0700

    Tao-Main-Acti

--- Movie-App/app/src/main/java/com/example/movieapp/MainActivity.java
+++ Movie-App/app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.movieapp;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void NowSlide(View view) {
+        Intent intent=new Intent(this,NowSlide.class);
+        startActivity(intent);
+
+    }
+    public void WillSlide(View view) {
+        Intent intent=new Intent(this,WillSlide.class);
+        startActivity(intent);
+    }
+    public void Login(View view) {
+    }
+    public void ListPhim(View view) {
     }
 }
