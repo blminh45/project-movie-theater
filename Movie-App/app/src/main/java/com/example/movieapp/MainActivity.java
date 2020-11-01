f16d125dbee36666591122c550d35f492b9d5d8f
Tuns <0306181290@caothang.edu.vn>
2020-11-01 15:43:43 +0700

    tao navbar

--- Movie-App/app/src/main/java/com/example/movieapp/MainActivity.java
+++ Movie-App/app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.movieapp;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 
 import android.app.ActivityOptions;
@@ -7,7 +9,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Pair;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,17 +20,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*Phan cua Tam*/
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(MainActivity.this,Notification.class);
-//                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this);
-//                startActivity(intent,options.toBundle());
-//                finish();
-//            }
-//        },200);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_login:
+                Intent intent1=new Intent(this,LoginActivity.class);
+                startActivity(intent1);;
+                return true;
+            case R.id.action_signup:
+                Intent intent2=new Intent(this,SignUpActivity.class);
+                startActivity(intent2);;
+                return true;
+            case R.id.action_list:
+                Intent intent3=new Intent(this,ListMovieActivity.class);
+                startActivity(intent3);;
+                return true;
+            case R.id.action_profile:
+                Intent intent4=new Intent(this,ProfileActivity.class);
+                startActivity(intent4);
+                return true;
+
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
     public void NowSlide(View view) {
         Intent intent=new Intent(this,NowSlide.class);
         startActivity(intent);
@@ -34,23 +65,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent=new Intent(this,WillSlide.class);
         startActivity(intent);
     }
-    public void Login(View view) {
-        Intent intent=new Intent(this,LoginActivity.class);
-        startActivity(intent);
-    }
-    public void ListPhim(View view) {
-        Intent intent=new Intent(this,ListMovieActivity.class);
-        startActivity(intent);
-    }
 
 
-    public void Singup(View view) {
-        Intent intent=new Intent(this,SignUpActivity.class);
-        startActivity(intent);
-    }
 
-    public void profile(View view) {
-        Intent intent=new Intent(this,ProfileActivity.class);
-        startActivity(intent);
-    }
 }
